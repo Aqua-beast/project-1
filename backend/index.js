903647044c57aabf73844796bbb1806524fffee0
= <=>
2024-06-07 08:03:35 +0530

    deploying

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,10 @@ app.use(express.json());
 // adding routes 
 app.use('/auth', authRouter);
 
+app.get('/', (req, res)=> {
+    res.status(200).json({message: "hello world"})
+})
+
 // setting up Environment Variables
 const port = process.env.PORT || 3000;
 const url = process.env.MONGO_DB_URL;
--- client/src/components/Home/Tracker.jsx
+++ client/src/components/Home/Tracker.jsx
@@ -2,7 +2,32 @@ import React from 'react';
 
 function Tracker() {
   return (
-    <div>Tracker Component</div>
+    <div className="container">
+      <div className="overview">
+        <div className="tracker">
+          <div className="tracker-content">
+            <p>PENDING TRACKERS</p>
+            <h3>45/60</h3>
+          </div>
+          <div className="icon">
+            <i className="fa-regular fa-bookmark"></i>
+          </div>
+        </div>
+        <div className="pending-review">
+          <div className="tracker-content">
+            <p>PENDING REVIEWS</p>
+            <h3>3</h3>
+          </div>
+          <div className="icon">
+            <i className="fa-regular fa-bookmark"></i>
+          </div>
+        </div>
+      </div>
+      <div className="searchbox">
+        <i className="fa-regular fa-bookmark"></i>
+        <input type="text" placeholder="Search for a business unit" />
+      </div>
+    </div>
   );
 }
 
